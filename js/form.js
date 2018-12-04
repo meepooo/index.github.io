8008233768797795f014ae300a6362d05ee314b6
Lucky <231876009@qq.com>
2018-12-04 23:33:32 +0800

    iframe
    iframe

--- js/form.js
+++ js/form.js
@@ -2,7 +2,9 @@ function clickSearch(){
 	var input = document.getElementById("head_search_input").value;
 	console.log(input);
 	window.event.returnValue=false;//阻止浏览器默认处理
-	window.location.href='https://www.baidu.com/s?wd='+input;
+	// window.location.href='https://www.baidu.com/s?wd='+input;
+	window.parent.location.href='https://www.baidu.com/s?wd='+input;
+	window.location.href='https://sylucky.github.io';
 }
 
 function enterIn(evt){
